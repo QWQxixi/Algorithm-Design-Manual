896f7e4afa42bc751a5468f92643d9a9924c2cba
qwq <2108680648@qq.com>
2024-10-27 22:04:37 +0800

    this is a test

---
+++ py_test.py
@@ -0,0 +1,45 @@
+
+# 输入
+'''
+    val=input([提示字符串])
+
+    input()函数将输入视作string类型，并返回string类型
+'''
+
+# 输出
+'''
+    print()
+'''
+
+# 分支
+'''
+    if 判断条件1:
+        
+    elif 判断条件2:
+        
+    else 判断条件3:
+'''
+
+# 循环
+'''
+    for i in range(start, stop, step):
+        
+    左闭右开
+
+'''
+
+# 列表
+'''
+    items = ['Python', 'Java', 'Go', 'Kotlin']
+    遍历
+    for index in range(len(items)):
+        print(items[index])
+
+    生成式创建
+
+'''
+
+# 元组——不可变类型容器
+'''
+    
+'''
